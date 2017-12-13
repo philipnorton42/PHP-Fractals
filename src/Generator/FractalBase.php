4d95985d3ce203d21e7b32825cbe58ca8ea6ab5e
Philip Norton <philipnorton42@gmail.com>
2017-12-13 22:33:19 +0000

    initial code for mandlebrot generation

--- src/Generator/FractalBase.php
+++ src/Generator/FractalBase.php
@@ -6,4 +6,63 @@ use Hashbangcode\Fractals\Utility\ComplexNumber;
 
 abstract class FractalBase implements FractalInterface {
 
+  protected $maxIteration;
+  protected $width;
+  protected $height;
+
+  /**
+   * @return mixed
+   */
+  public function getMaxIteration()
+  {
+    return $this->maxIteration;
+  }
+
+  /**
+   * @param mixed $maxIteration
+   */
+  public function setMaxIteration($maxIteration)
+  {
+    $this->maxIteration = $maxIteration;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getWidth()
+  {
+    return $this->width;
+  }
+
+  /**
+   * @param mixed $width
+   */
+  public function setWidth($width)
+  {
+    $this->width = $width;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getHeight()
+  {
+    return $this->height;
+  }
+
+  /**
+   * @param mixed $height
+   */
+  public function setHeight($height)
+  {
+    $this->height = $height;
+  }
+
+  public function __construct($height, $width, $maxiterations = 200)
+  {
+    $this->setHeight($height);
+    $this->setWidth($width);
+
+    $this->setMaxIteration($maxiterations);
+  }
 }
--- src/Generator/Mandlebrot.php
+++ src/Generator/Mandlebrot.php
@@ -2,9 +2,63 @@
 
 namespace Hashbangcode\Fractals\Generator;
 
-class Mandlebrot extends FractalBase {
+class Mandlebrot extends FractalBase
+{
 
-  public function generate() {
+  public function generate()
+  {
+    // Set the image width and height
+ //   $width = 2000;
+//    $height = 2000;
+
+    // Set zoom factor
+    $ZoomFactor = 2;
+
+    // These two variables set the center point of the mandlebrot set
+    $xShift = 0;
+    $yShift = 0;
+
+    // Calculate bounds
+    $MinReal = ($this->getMinX() * $ZoomFactor) + $xShift; // x
+    $MaxReal = ($this->getMaxX() * $ZoomFactor) + $xShift; // x
+    $MinImaginary = (-1.2 * $ZoomFactor) + $yShift; // y
+    $MaxImaginary = $MinImaginary + ($MaxReal - $MinReal) * ($this->getHeight() / $this->getWidth()); // y
+
+    // Pre calculate factors
+    $RealFactor = ($MaxReal - $MinReal) / ($this->getWidth() - 1);
+    $ImaginaryFactor = ($MaxImaginary - $MinImaginary) / ($this->getHeight() - 1);
+
+    // Set maximum number of iterations to count Z
+    // The higher the number the more iterations are done before the value is assumed to escape the more accurate the image and the longer it takes to render it. A value of 30 will create a simple set in a few seconds, whereas a value of 1000 will create a large set, but is very, very slow. How long the set takes to generate will also depend on the size of the image created.
+    //$maxIteration = 10000;
+
+    for ($y = 0; $y < $this->getHeight(); ++$y) {
+      $cImaginary = $MaxImaginary - ($y * $ImaginaryFactor);
+
+      for ($x = 0; $x < $this->getWidth(); ++$x) {
+
+        $cReal = $MinReal + ($x * $RealFactor);
+
+        $ZReal = $cReal;
+        $ZImaginary = $cImaginary; // Set Z = c
+
+        // Calculate Z
+        for ($i = 0; $i < $this->getMaxIteration(); ++$i) {
+
+          $ZReal2 = $ZReal * $ZReal;
+          $ZImaginary2 = $ZImaginary * $ZImaginary;
+
+          // if the value 'escapes' then break out of this loop
+          if ($ZReal2 + $ZImaginary2 > 4) {
+            break;
+          }
+
+          $ZImaginary = 2 * $ZReal * $ZImaginary + $cImaginary;
+          $ZReal = $ZReal2 - $ZImaginary2 + $cReal;
+        }
+      }
+    }
+
 
   }
 }
