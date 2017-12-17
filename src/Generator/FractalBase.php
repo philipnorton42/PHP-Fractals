bd831442da64da62a39b794e52acf56ffe2564c5
Philip Norton <philipnorton42@gmail.com>
2017-12-17 22:42:01 +0000

    added some manipulation features to the core fractal generation classs, also fleshed out the mandlebrot set with these manipulation functions

--- src/Generator/FractalBase.php
+++ src/Generator/FractalBase.php
@@ -11,10 +11,79 @@ abstract class FractalBase implements FractalInterface {
   protected $height;
   protected $pixels = [];
 
+  protected $zoom;
+
+  protected $moveX = -0.5;
+  protected $moveY = 0;
+
+  /**
+   * @return int
+   */
+  public function getMoveX()
+  {
+    return $this->moveX;
+  }
+
+  /**
+   * @param int $moveX
+   */
+  public function setMoveX($moveX)
+  {
+    $this->moveX = $moveX;
+  }
+
+  /**
+   * @return int
+   */
+  public function getMoveY()
+  {
+    return $this->moveY;
+  }
+
+  /**
+   * @param int $moveY
+   */
+  public function setMoveY($moveY)
+  {
+    $this->moveY = $moveY;
+  }
+
+  /**
+   * @return float
+   */
+  public function getZoom()
+  {
+    return $this->zoom;
+  }
+
+  /**
+   * @param float $zoom
+   */
+  public function setZoom($zoom)
+  {
+    $this->zoom = $zoom;
+  }
+
+  /**
+   * Set a pixel to a particular value.
+   *
+   * @param int $y
+   *   The y coordinate.
+   * @param int $x
+   *   The x coordinate.
+   * @param int $value
+   *   The value to set.
+   */
   public function setPixel($y, $x, $value) {
     $this->pixels[$y][$x] = $value;
   }
 
+  /**
+   * Get the entire pixel array.
+   *
+   * @return array
+   *   The entire pixel array.
+   */
   public function getPixels() {
     return $this->pixels;
   }
@@ -67,11 +136,32 @@ abstract class FractalBase implements FractalInterface {
     $this->height = $height;
   }
 
-  public function __construct($height, $width, $maxiterations = 200)
+  /**
+   * FractalBase constructor.
+   * @param $height
+   * @param $width
+   * @param int $maxiterations
+   */
+  public function __construct($height, $width, $zoom = 1, $maxiterations = 100)
   {
     $this->setHeight($height);
     $this->setWidth($width);
-
+    $this->setZoom($zoom);
     $this->setMaxIteration($maxiterations);
   }
+
+  /**
+   *
+   */
+  public function __destruct()
+  {
+   unset($this->height);
+   unset($this->width);
+   unset($this->maxIteration);
+   unset($this->pixels);
+   unset($this->moveX);
+   unset($this->moveY);
+   unset($this->zoom);
+  }
+
 }
--- src/Generator/FractalInterface.php
+++ src/Generator/FractalInterface.php
@@ -3,5 +3,12 @@
 namespace Hashbangcode\Fractals\Generator;
 
 interface FractalInterface {
+
+  /**
+   * Generate the fractal set.
+   *
+   * @return array
+   *   The set.
+   */
   public function generate();
 }
