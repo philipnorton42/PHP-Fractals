b56eb22eca98a1bb4abef0b9579f5ca6abdd1973
Philip Norton <philipnorton42@gmail.com>
2017-12-18 22:30:29 +0000

    added the Palette class to the image decorator class

--- src/Decorator/ImageDecorator.php
+++ src/Decorator/ImageDecorator.php
@@ -3,6 +3,7 @@
 namespace Hashbangcode\Fractals\Decorator;
 
 use Hashbangcode\Fractals\Generator\FractalInterface;
+use Hashbangcode\Fractals\Utility\Palette;
 
 class ImageDecorator extends FractalDecoratorBase {
 
@@ -48,20 +49,22 @@ class ImageDecorator extends FractalDecoratorBase {
     $im = imagecreatetruecolor($this->getFractal()->getWidth(), $this->getFractal()->getHeight());
     $background_color = imagecolorallocate($im, 255, 255, 255);
     imagefilledrectangle($im, 0, 0, $this->getFractal()->getHeight(), $this->getFractal()->getWidth(), $background_color);
-    $colorFactor = log($this->getFractal()->getWidth());
+    //$colorFactor = log($this->getFractal()->getWidth());
+
+    $palette = new Palette($this->getFractal()->getMaxIteration());
 
     $pixels = $this->getFractal()->getPixels();
 
     foreach ($pixels as $y => $value) {
       foreach ($pixels[$y] as $x => $value) {
-
-      if ($value == $this->getFractal()->getMaxIteration()) {
-        $colour = imagecolorallocate($im, 0, 0, 0);
-      } else {
-        $colorValue = log($value) / $colorFactor * 255;
-        $colour = imagecolorallocate($im, $colorValue, $colorValue, $colorValue);
-      }
-      imagesetpixel($im, $x, $y, $colour);
+        if ($value == $this->getFractal()->getMaxIteration()) {
+          $colour = imagecolorallocate($im, 0, 0, 0);
+        } else {
+          $colorValue = $palette[$value];
+          //$colorValue = log($value) / $colorFactor * 255;
+          $colour = imagecolorallocate($im, $colorValue[0], $colorValue[1], $colorValue[2]);
+        }
+        imagesetpixel($im, $x, $y, $colour);
       }
     }
 
