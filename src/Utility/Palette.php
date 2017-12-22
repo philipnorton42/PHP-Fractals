0aa5a60293cf5e93f68e1c9f9279e3829a8e8dc7
Philip Norton <philipnorton42@gmail.com>
2017-12-22 19:14:10 +0000

    created a better colour Palette system

--- src/Utility/Palette.php
+++ src/Utility/Palette.php
@@ -17,11 +17,17 @@ class Palette implements \ArrayAccess {
     $this->maxIterations = $maxIterations;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function offsetExists($offset)
   {
     return $offset < $this->maxIterations;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function offsetGet($offset)
   {
     if (isset($this->palette[$offset])) {
@@ -35,32 +41,37 @@ class Palette implements \ArrayAccess {
       return $this->palette[$offset];
     }
 
-    // Create a colour.
-    /*$colour = [
-      $offset / $this->maxIterations * 255,
-      0,
-      log($offset) / log($this->maxIterations) * 255,
-    ];*/
-
+    // Figure out the percentage of escape.
     $value = $offset / $this->maxIterations * 255;
-
+    
+    // Create the palette.
     $colour = [
-      abs(sin(0.016 * $value + 4) * 230 + 25),
-      abs(sin(0.013 * $value + 2) * 230 + 25),
-      abs(sin(0.01 * $value + 1) * 230 + 25),
+      // Red.
+      abs(sin(0.016 * $value + 6) * 230 + 20),
+      // Green.
+      abs(sin(0.013 * $value + 6) * 230 + 25),
+      // Blue.
+      abs(sin(0.01 * $value + 6) * 230 + 25),
     ];
 
     // Store the colour in the palette.
     $this->palette[$offset] = $colour;
 
+    // Return the pallet colour.
     return $this->palette[$offset];
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function offsetSet($offset, $value)
   {
     return;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function offsetUnset($offset)
   {
     return;
